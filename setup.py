6e99d345fc69e5b2f3c2b36b13cb6ee34215a4c5
parthsolanke <parthsolanke2002@gmail.com>
2024-05-09 21:42:25 +0530

    refactor: update setup.py with package description and license

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mailsensei',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description="""
+    MailSensei is a Python package
+    that uses the Hugging Face APIs
+    and LLMs to summarize and simplify E-mails.,
+    """
+)
